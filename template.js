a8d09dfa88bc13f945e6c058151c5d0d4895cf7c
altspage <66216786+altspage@users.noreply.github.com>
2020-06-06 16:17:59 +0200

    Update template.js

--- template.js
+++ template.js
@@ -111,14 +111,3 @@ var rnd = Math.ceil(Math.random() * NumberOfWords)
 // Display the word inside the text box
 frm.WordBox.value = words[rnd]
 }
-<script type="text/javascript">
-    var vglnk = {key: 'b126d006d7abe1fd62804f1be9e3ce0d'};
-    (function(d, t) {
-        var s = d.createElement(t);
-            s.type = 'text/javascript';
-            s.async = true;
-            s.src = '//cdn.viglink.com/api/vglnk.js';
-        var r = d.getElementsByTagName(t)[0];
-            r.parentNode.insertBefore(s, r);
-    }(document, 'script'));
-</script>
